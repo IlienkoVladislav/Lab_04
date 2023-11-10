34b1e0d7efb7ae3ce7a57a8a6347d9ae99bde82f
IlienkoVladislav <147603955+IlienkoVladislav@users.noreply.github.com>
2023-11-10 09:04:38 +0200

    Add files via upload

---
+++ Task_01/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+    interface ISearchable
+    {
+        IEnumerable<Product> SearchByPrice(double maxPrice);
+        IEnumerable<Product> SearchByCategory(string category);
+    }
+    class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<Order> PurchaseHistory { get; set; } = new List<Order>();
+    }
+
+    class Order
+    {
+        public List<Product> Products { get; set; }
+        public List<int> Quantities { get; set; }
+        public double TotalPrice => Products.Zip(Quantities, (product, quantity) => product.Price * quantity).Sum();
+        public bool IsCompleted { get; set; }
+    }
+
+    class Shop : ISearchable
+    {
+        public List<User> Users { get; set; } = new List<User>();
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Order> Orders { get; set; } = new List<Order>();
+
+        public IEnumerable<Product> SearchByPrice(double maxPrice)
+        {
+            return Products.Where(product => product.Price <= maxPrice);
+        }
+
+        public IEnumerable<Product> SearchByCategory(string category)
+        {
+            return Products.Where(product => product.Category == category);
+        }
+
+
+        public void PlaceOrder(User user, List<Product> products, List<int> quantities)
+        {
+            var order = new Order
+            {
+                Products = products,
+                Quantities = quantities,
+                IsCompleted = false
+            };
+
+            user.PurchaseHistory.Add(order);
+            Orders.Add(order);
+        }
+
+        public void CompleteOrder(Order order)
+        {
+            order.IsCompleted = true;
+        }
+    }
+}
---
+++ Task_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var shop = new Shop();
+
+            var user1 = new User { Username = "user1", Password = "pass1" };
+            var user2 = new User { Username = "user2", Password = "pass2" };
+
+            shop.Users.Add(user1);
+            shop.Users.Add(user2);
+
+            var product1 = new Product { Name = "Product1", Price = 60.5, Category = "Category1" };
+            var product2 = new Product { Name = "Product2", Price = 36.0, Category = "Category2" };
+
+            shop.Products.Add(product1);
+            shop.Products.Add(product2);
+
+            var order1Products = new List<Product> { product1, product2 };
+            var order1Quantities = new List<int> { 2, 1 };
+
+            shop.PlaceOrder(user1, order1Products, order1Quantities);
+
+            var order2Products = new List<Product> { product2 };
+            var order2Quantities = new List<int> { 3 };
+
+            shop.PlaceOrder(user2, order2Products, order2Quantities);
+
+            foreach (var order in shop.Orders)
+            {
+                Console.WriteLine($"Order Total Price: {order.TotalPrice}, Completed: {order.IsCompleted}");
+            }
+        }
+    }
+}
+
+
+
+
